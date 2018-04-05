2515c1570ce94097bc558252c509772f95d8a85f
Nathan <nathanbooth@gmail.com>
2018-04-05 16:33:35 -0600

    Create vncedit.ps1

---
+++ vncedit.ps1
@@ -0,0 +1,6 @@
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\TightVNC\Server\" -Name "Password"
+$regpath = "HKLM:\SOFTWARE\TightVNC\Server\"
+$Name = "UseVncAuthentication"
+$value = "0"
+New-ItemProperty -Path $regpath -Name $Name -Value $value -PropertyType DWORD -Force | Out-Null
+& 'C:\Program Files\TightVNC\tvnserver.exe' -controlservice -reload
